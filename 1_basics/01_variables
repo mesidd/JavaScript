0e8a2294431fdd4ce929d4ebbbf4fdfd689deecf
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-21 06:37:49 +0000

    variables done

---
+++ 1_basics/01_variables
@@ -0,0 +1,14 @@
+const account_id = 2282002
+let account_email="sid@gmail.com"
+var account_password="12345"
+account_city = " Vizag"
+let account_state;
+
+// account_id=344 // not allowed
+
+console.table([account_id,account_email,account_password,account_city,account_state])
+
+/* 
+Not use var because of block and functional scope
+Use const and let;
+*/
---
+++ 1_basics/02_data_types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert("hello")
+
+let a="hello"
+console.log(a);
+
+// alert(2+3) // we're using nodejs not browser
+
+console.log(3+3) // code readability should be high
+
+//number ==> 2^53
+//bigint
+//string ==>""
+//boolean ==>true/false
+//null ==> standalone value 
+// undefined ==> value not assigned
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+

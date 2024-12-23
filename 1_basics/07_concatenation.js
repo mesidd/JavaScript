b054a9dcd276dd23e29eee9d4f8baea4e046006d
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-23 19:05:56 +0000

    string done

---
+++ 1_basics/07_concatenation.js
@@ -0,0 +1,16 @@
+// const name ="sidd"
+// const title="sharma"
+// console.log(name + title)
+
+// use backtick
+
+// console.log(`Full name is : ${name} ${title}`);
+
+// new object name
+
+
+
+
+
+
+
---
+++ 1_basics/08_string_fn.js
@@ -0,0 +1,32 @@
+const game_name = new String('siddharth')
+
+// console.log(game_name);
+// console.log(game_name.length);
+// console.log(game_name.toUpperCase());
+// console.log(game_name.charAt(2));
+// console.log(game_name.indexOf('d'));
+// console.log(game_name.substring(0,5))
+
+// slice
+
+// const another_string=game_name.slice(-5,-1)
+// console.log(another_string);
+
+// const name="   krrish  "
+// console.log(name.trimEnd()); // trimStart trimEnd trim
+
+// const url="https//www.google%20.com"
+// console.log(url.replace('%20.',"."));
+
+// console.log(url.includes('google'));
+
+// const info="Hey I am Sid! What's up?"
+// split_list = info.split(' ')
+// console.log(split_list[2]);
+
+
+const add="vizag-visalakshinagar-900"
+add_list=add.split("-") // split
+console.log(add_list[2])
+
+// Strings are very important specifically the methods of it.

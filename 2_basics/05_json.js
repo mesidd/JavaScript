1bfc8b75f80f056209eeea42ea04b4038ced8935
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-24 09:16:18 +0000

    json

---
+++ 2_basics/05_json.js
@@ -0,0 +1,66 @@
+{
+    "results": [
+      {
+        "gender": "male",
+        "name": {
+          "title": "Mr",
+          "first": "Vlad",
+          "last": "Senkivskiy"
+        },
+        "location": {
+          "street": {
+            "number": 8979,
+            "name": "Mikoli Gulaka"
+          },
+          "city": "Svyatogirsk",
+          "state": "Ivano-Frankivska",
+          "country": "Ukraine",
+          "postcode": 68940,
+          "coordinates": {
+            "latitude": "73.8783",
+            "longitude": "-124.8742"
+          },
+          "timezone": {
+            "offset": "-3:00",
+            "description": "Brazil, Buenos Aires, Georgetown"
+          }
+        },
+        "email": "vlad.senkivskiy@example.com",
+        "login": {
+          "uuid": "2c5e4c5a-98a6-422e-9674-8160ed586d87",
+          "username": "heavyfish550",
+          "password": "goldwing",
+          "salt": "MFQylh4f",
+          "md5": "5c6bb9ed9dfab2b56b2844f1546d2935",
+          "sha1": "9e90fd6877419baa3b90335954a83ea905573da7",
+          "sha256": "f355078f7a0cb02dd5f7988ca61505a91ee7a71599634c15df297e62256783e2"
+        },
+        "dob": {
+          "date": "1951-07-28T10:30:50.439Z",
+          "age": 73
+        },
+        "registered": {
+          "date": "2010-02-26T05:45:17.129Z",
+          "age": 14
+        },
+        "phone": "(099) V35-3854",
+        "cell": "(068) F93-4047",
+        "id": {
+          "name": "",
+          "value": null
+        },
+        "picture": {
+          "large": "https://randomuser.me/api/portraits/men/2.jpg",
+          "medium": "https://randomuser.me/api/portraits/med/men/2.jpg",
+          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/2.jpg"
+        },
+        "nat": "UA"
+      }
+    ],
+    "info": {
+      "seed": "899a40636d103ce7",
+      "results": 1,
+      "page": 1,
+      "version": "1.4"
+    }
+  }

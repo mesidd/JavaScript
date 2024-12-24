06034a0624daa5c7cbdc4e584561671cb681a660
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-24 06:51:14 +0000

    array

---
+++ 2_basics/01_array.js
@@ -0,0 +1,21 @@
+const myarr=[1,2,3,4]
+// console.log(myarr);
+
+
+// myarr.unshift(9) // add on the front
+// console.log(myarr);
+
+// myarr.shift() // remove from the first
+// console.log(myarr);
+
+// const newarr=myarr.join() // string
+// console.log((newarr));
+
+ const myarr1=myarr.slice(1,3) // last excluded
+ console.log((myarr1));
+
+ const myarr2=myarr.splice(1,3) // last included & changed permanently
+ console.log((myarr2));
+ console.log(myarr)
+ 
+ 
---
+++ 2_basics/02_array_adv.js
@@ -0,0 +1,34 @@
+// const arr1=["hey","hello","hi"]
+// const arr2=["vizag","rnc","hyd"]
+
+// arr1.push(arr2)
+// console.log((arr1));
+
+// arr1.concat(arr2)
+// console.log(arr1)
+
+// const arr3=arr1.concat(arr2)
+// console.log(arr3);
+
+// const arr4=[...arr1,...arr2]
+// console.log(arr4);
+
+// const arr5=[1,2,3,[4,5,6],7,[8,9]]
+// const arr6=arr5.flat(Infinity)
+// console.log(arr6)
+
+
+// console.log(Array.from("siddharth"))
+
+// console.log(Array.from({name : "Siddharth"})) // empty array- no specification
+
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
+// combine variable and form array
+
+

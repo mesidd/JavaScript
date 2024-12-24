546917b3834028462be36b55a919ecf7cac01f32
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-24 10:34:07 +0000

    function

--- 2_basics/06_function.js
+++ 2_basics/06_function.js
@@ -22,17 +22,41 @@ function addno(n1,n2){ // n1,n2 - parameters
 
 // !userename -- false
 // username = 'sam' --> default value
-function login(userename){
-    if (userename === undefined){
-        console.log("Enter username");
-        return 
-    }
-    return `${userename} logged in.`
-}
-console.log(login("sid"));
-console.log(login(""));
-console.log(login());
+// function login(userename){
+//     if (userename === undefined){
+//         console.log("Enter username");
+//         return 
+//     }
+//     return `${userename} logged in.`
+// }
+// console.log(login("sid"));
+// console.log(login("")); 
+// console.log(login());
 
+
+// function calculate_cart(...num){ //...num
+//     return num
+// }
+// console.log(calculate_cart(700)); // 700
+// console.log(calculate_cart(700,800)); // 700
+// console.log(calculate_cart(700,800)); // ...num
+
+
+// function list_value(val1,val2,...num){
+//     return num
+// }
+// console.log(list_value(700,800,900)) // 700,800 - val1,val2 - num=900
+
+
+ function handle_object(user){
+    console.log(`Username is : ${user.username}, City : ${user.city}`);
+ }
+const user ={ 
+    username : "roy",
+    city : "nyc"
+}
+
+ handle_object({"username": "rohan", "city" : " bangkok"})
 
 
 

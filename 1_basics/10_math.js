07ca4dbe544db2bccd90443ca6f2271890ae7b2e
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-24 04:57:37 +0000

    basics

--- 1_basics/10_math.js
+++ 1_basics/10_math.js
@@ -4,4 +4,3 @@ console.log((Math.random()*10)+1)
 const min=10
 const max=20
 console.log(max-min);
-
---
+++ 1_basics/11_date.js
@@ -0,0 +1,18 @@
+
+
+// let my_date = new Date()
+// console.log(my_date.toDateString())
+// console.log(my_date.toLocaleString())
+// console.log(my_date.toJSON())
+// console.log(my_date.toTimeString())
+// console.log(my_date.toISOString())
+
+// typeof my_date = object
+
+let mycreaeddate= new Date (2024,11,24)
+let mytimestamp = Date.now()
+console.log(mytimestamp);
+console.log(Math.floor(mytimestamp/1000))
+// console.log((mycreaeddate.getTime()));
+
+

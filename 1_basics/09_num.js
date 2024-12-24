99889486569f1869d68df0be61c6cae4f47e1e07
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-24 04:14:12 +0000

    number

---
+++ 1_basics/09_num.js
@@ -0,0 +1,17 @@
+const number=100
+console.log(number)
+
+const rem = new Number(600)
+console.log(rem)
+
+console.log(number.toFixed(2)) // ecommerce
+
+const other=1123.9876
+console.log(other.toPrecision(3))
+
+const zero=1000000
+console.log(zero.toLocaleString())
+console.log(zero.toLocaleString('en-In'));
+
+
+

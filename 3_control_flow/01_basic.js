8f40d157e03004078d6ba53792cb7352f1048ddf
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-25 06:45:02 +0000

    control

---
+++ 3_control_flow/01_basic.js
@@ -0,0 +1,25 @@
+// if - else if - else
+
+// a || b || c
+
+// switch --> if break not used then all case will be executed without further matching except default 
+
+// falsy values-> false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy-> "0",'false," ",[], {}, function(){}
+
+/* Check Array if it's empty:
+        if(array.length === 0){
+        console.log("Array is empty")}
+
+    Check if object is empty:
+    const emptyObj={}
+        if(Object.keys(emptyObj).lenght ===0 ){
+        console.log("Object is empty")};
+Imp. Case:
+
+1. false == 0 // true
+2. false == '' // true
+3. 0 == '' // true
+
+*/

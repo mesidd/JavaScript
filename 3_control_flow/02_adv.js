80a0be466a81d6816f28b7fd79ad6d55f2f49e63
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2024-12-25 07:33:31 +0000

    control flow

---
+++ 3_control_flow/02_adv.js
@@ -0,0 +1,20 @@
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+
+// val1 = 5 ?? 10
+// console.log(val1)
+
+// val1 = null ?? 10
+// console.log(val1)
+
+// val1 = undefined ?? 5
+// console.log(val1)
+
+val1 = null ?? 10 ?? 5
+console.log(val1)
+
+// Ternart Operator:
+

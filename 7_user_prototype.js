473f718b98a47dae2dc1985a3c9abf20e4b2e60f
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-03-08 13:25:13 +0530

    myFilter & myMap Just For Reference

--- 7_user_prototype.js
+++ 7_user_prototype.js
@@ -4,6 +4,7 @@ const n = arr.map((e)=> e*2);
 
 console.log(n);
 
+// myMap
 if(!Array.prototype.myMap) {
   Array.prototype.myMap = function(userFn) {
     const arr = this
@@ -18,3 +19,21 @@ if(!Array.prototype.myMap) {
 
 const n1 = arr.myMap( (e) => e * 3 );
 console.log(n1);
+
+// myFilter
+
+if(!Array.prototype.myFilter){
+  Array.prototype.myFilter = function(userFn){
+    const result = []
+    for ( let i = 0; i < this.length; i++){
+      if ( userFn(this[i]) == 1){
+        result.push(this[i]);
+      }
+    }
+    return result
+  }
+}
+
+const n5 = arr.myFilter((e)=> e %2 == 0)
+
+console.log(n5);

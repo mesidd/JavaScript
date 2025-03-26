f4d95d6141d45b9bb8e5422c33215e9793b50424
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-03-26 15:58:15 +0530

    Curry Method

--- 16_currying.js
+++ 16_currying.js
@@ -10,3 +10,8 @@ let step1 = sendAutoEmail("Alpha@gmail.com")
 let step2 = step1("Order Confirmation")
 
 step2("Hey Alpha, your product order is confirmed!")
+
+
+// Another Modern Method
+
+const sendEmail = (to) => (subject) => (body) =>  console.log(`Sending email to ${to}\nwith subject ${subject}\n${body}`);

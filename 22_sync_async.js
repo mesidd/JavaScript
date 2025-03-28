60e247cdabe92b96c4f48328b8b3e6e6e3e1c094
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-03-28 16:02:32 +0530

    Blocking vs Non-Blocking

---
+++ 22_sync_async.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+// Blocing .... Sync
+
+console.log('1');
+const result = fs.readFileSync('./info.txt','utf-8');
+console.log(result);
+console.log('2\n');
+// Output : 1 "..." 2
+
+
+// Non-Blocking .... Async
+
+console.log('1');
+fs.readFile('./info.txt','utf-8',(err, res)=>{
+  console.log(res);
+})
+console.log('2');
+// Output : 1 2 "...."

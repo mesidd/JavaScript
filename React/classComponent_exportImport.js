34fc1702483100b7fbd162c12ecdc4b7f72c3d8f
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-04 15:29:01 +0530

    Basic Component & Import Export

---
+++ React/classComponent_exportImport.js
@@ -0,0 +1,21 @@
+
+// Same Functionality
+
+  return <h1> Wanna Go Dubai </h1>
+
+  return React.createElement('h1', null, 'Wannna Go Dubai')
+
+  return React.createElement('div', null, React.createElement('h1', null, 'Wanna Go Dubai'))
+
+
+// Named Export
+  export const Welcome = () => <h1> Welcome </h1>  
+  import { Welcome }
+
+    
+// Default Export
+  function Welcome () {
+    return <h1> Welcome </h1>
+  }
+  export default Welcome 
+  import Welcome

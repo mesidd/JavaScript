b51feed776ca18e2b4be2f69b63995e421283c2a
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-06 14:34:51 +0530

    custom hook

--- React/customHook.js
+++ React/customHook.js
@@ -31,7 +31,7 @@ function HookTest(){
 
   return(
     <div>
-      <h1>Coutnt : {count} </h1>
+      <h1>Count : {count} </h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button>

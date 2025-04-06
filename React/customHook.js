7cf6269f4d8bca503f027c2bedb5d18933e21dc2
Siddhartha Sharma <160341360+mesidd@users.noreply.github.com>
2025-04-06 14:32:59 +0530

    Custom Hook (Counter)

---
+++ React/customHook.js
@@ -0,0 +1,42 @@
+// CustomHook Definition
+import  {useState} from "react";
+
+function CustomHook(){
+
+  const [ count, setCount ] = useState(0)
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  return [increment, decrement, reset, count]
+
+}
+
+export default CustomHook
+
+// Usage
+import React from "react";
+import CustomHook from "./CustomHook";
+
+function HookTest(){
+
+  const [increment, decrement, reset, count] = CustomHook()
+
+  return(
+    <div>
+      <h1>Coutnt : {count} </h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+}
+
+export default HookTest
